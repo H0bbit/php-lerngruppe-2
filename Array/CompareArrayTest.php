fdf3dfed47fbc0b1f8ae68d704933373d0dc738f
H0bbit <patrick.lessmann@googlemail.com>
2012-11-12 17:27:08 +0100

    Created Testfile for Package 2 - Compare Arrays

---
+++ Array/CompareArrayTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * compare sort functions
+ *
+ * @author André
+ */
+class CompareArrayTest extends PHPUnit_Framework_TestCase
+{
+    public function testArrayDiffAssoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    public function testArrayDiffKey()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayDiffUassoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayDiffUkey()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayDiff()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayIntersectAssoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayIntersectKey()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayIntersectUassoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayIntersectUkey()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayIntersect()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayUdiffAssoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayUdiffUassoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayUdiff()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayUintersectAssoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayUintersectUassoc()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testArrayUintersect()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    public function testInArray()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
